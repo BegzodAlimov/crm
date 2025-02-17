a9ee753a41d2835c133c2860e033bb899b94ed78
begzod-alimov <begzodalimov614@gmail.com>
2025-02-17 15:13:06 +0500

    push all

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('token/', TokenRefreshAPIView.as_view(), name='token'),
-    path('current_user/', CurrentUserAPIView.as_view(), name='current_user'),
+    path('me/', CurrentUserAPIView.as_view(), name='me'),
 ]
